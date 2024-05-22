4332b410ea8cfc536288c24a60509eaff7767527
Matthew Johnston <warmwaffles@gmail.com>
2024-05-22 12:24:17 -0500

    Add xit to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -875,6 +875,15 @@ require("lazy").setup({
             --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
         end,
     },
+    {
+        "synaptiko/xit.nvim",
+        opt = true,
+        ft = "xit",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("xit").setup()
+        end
+    },
 
     -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
