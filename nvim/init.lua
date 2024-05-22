9c44c3ab0535a10e7e7259122960bef56974b594
Matthew Johnston <warmwaffles@gmail.com>
2024-05-22 11:01:11 -0500

    Swap to bluloco-light for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -778,13 +778,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'Shatur/neovim-ayu',
+    'uloco/bluloco.nvim',
+    lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
+    dependencies = { 'rktjmp/lush.nvim' },
+    config = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'ayu-light', 'ayu-mirage', 'ayu-dark', or 'ayu'.
-      vim.cmd.colorscheme 'ayu-light'
+      -- any other, such as 'bluloco-light' or 'bluloco-dark'.
+      vim.cmd.colorscheme 'bluloco-light'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
